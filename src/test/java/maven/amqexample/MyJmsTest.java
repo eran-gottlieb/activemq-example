bb096d0498225b540b1ec6a8c7d6c460e1d85dc9
Eran Gottlieb <eran.gottlieb@outlook.com>
2023-02-25 11:37:21 +0200

    Clean code

--- src/test/java/maven/amqexample/MyJmsTest.java
+++ src/test/java/maven/amqexample/MyJmsTest.java
@@ -89,7 +89,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
     @ParameterizedTest
     @ValueSource(ints = {1000})
-    public void simpleTest(int num) throws Exception {        System.out.println("Test starting...");
+    public void simpleTest(int num) throws Exception {        
+        System.out.println("Test starting...");
         System.out.println(count() + " browse messages in queue");
         long t1 = System.currentTimeMillis();
         sendMessages(num);
