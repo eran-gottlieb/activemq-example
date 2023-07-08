a1080b5dce33ccda89cc138db3e4bbdd05c05c36
Eran Gottlieb <eran.gottlieb@outlook.com>
2023-07-08 12:55:33 +0300

    Update ActiveMQ 5.18.2

--- src/test/java/maven/amqexample/MyJmsTestIT.java
+++ src/test/java/maven/amqexample/MyJmsTestIT.java
@@ -1,28 +1,19 @@
 package maven.amqexample;
 
-import java.text.MessageFormat;
-import java.util.Enumeration;
-
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.QueueBrowser;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.jms.core.BrowserCallback;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.*;
+import java.text.MessageFormat;
+import java.util.Enumeration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Simple test case that sends and receives messages to/from a JMS broker.
@@ -51,46 +42,46 @@ import org.springframework.jms.core.MessageCreator;
     //@Disabled
     @ParameterizedTest
     @ValueSource(ints = {1,10,100})
-    public void someIntegrationTest(int num) throws Exception {
-        
+    public void someIntegrationTest(int num) throws JMSException {
         System.out.println("Test starting...");
         sendMessages(num);
         browseMessages(num);
         receiveMessages(num);
         System.out.println("Test done!");
-        
     }
-
+    /**
+     * 
+     * @param num number of sent messages.
+     */
     protected void sendMessages(int num) {
         for (int i = 1; i <= num; i++) {
             final int theMessageIndex = i;
             final String theMessageString = "Message: " + theMessageIndex;
             // System.out.println("Sending message with text: " + theMessageString);
 
-            mJmsTemplate.send(new MessageCreator() {
-                public Message createMessage(Session inJmsSession) throws JMSException {
-                    TextMessage theTextMessage = inJmsSession.createTextMessage(theMessageString);
-                    theTextMessage.setIntProperty("messageNumber", theMessageIndex);
+            mJmsTemplate.send(inJmsSession -> {
+                TextMessage theTextMessage = inJmsSession.createTextMessage(theMessageString);
+                theTextMessage.setIntProperty("messageNumber", theMessageIndex);
 
-                    return theTextMessage;
-                }
+                return theTextMessage;
             });
         }
     }
 
-    public void browseMessages(int expected) throws JMSException {
-
-        int actual = mJmsTemplate.browse(new BrowserCallback<Integer>() {
-            public Integer doInJms(final Session session, final QueueBrowser browser) throws JMSException {
-                Enumeration<?> enumeration = browser.getEnumeration();
-                int counter = 0;
-                while (enumeration.hasMoreElements()) {
-                    Message msg = (Message)enumeration.nextElement();
-                    // System.out.println(MessageFormat.format("\tFound : {0}", msg));
-                    counter += 1;
-                }
-                return counter;
+    /**
+     * 
+     * @param expected number of messages in queue
+     */
+    public void browseMessages(int expected) {
+        int actual = mJmsTemplate.browse((session, browser) -> {
+            Enumeration<?> enumeration = browser.getEnumeration();
+            int counter = 0;
+            while (enumeration.hasMoreElements()) {
+                Message msg = (Message) enumeration.nextElement() ;
+                System.out.println(MessageFormat.format("\tFound : {0}", msg));
+                counter += 1;
             }
+            return counter;
         });
 
         if (actual == 0)
@@ -100,23 +91,27 @@ import org.springframework.jms.core.MessageCreator;
         else if (actual > 1)    
             System.out.println(MessageFormat.format("There are {0} messages", actual));
 
-        //Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
-    protected void receiveMessages(int expected) throws Exception {
+    /**
+     * 
+     * @param expected num of messages to receive
+     */
+    protected void receiveMessages(int expected) throws JMSException {
         int actual = 0;
         Message theReceivedMessage = mJmsTemplate.receive();
 
         while (theReceivedMessage != null) {
             if (theReceivedMessage instanceof TextMessage) {
                 actual++;
-                // final TextMessage theTextMessage = (TextMessage)theReceivedMessage;
-                // System.out.println("Received a message with text: " + theTextMessage.getText());
+                final TextMessage theTextMessage = (TextMessage)theReceivedMessage;
+                System.out.println("Received a message with text: " + theTextMessage.getText());
             }
 
             theReceivedMessage = mJmsTemplate.receive();
         }
-        //Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
         System.out.println("All messages received!");
     }
 }
